bb746dd7825e60d4dd3cd05c1d8eff349fb391a2
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-01-18 20:07:16 +0100

    code refactoring

--- js/script.js
+++ js/script.js
@@ -1,35 +1,42 @@
-let inputAmountElement = document.querySelector(".js-inputAmount");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
-let selectCurrencyElement = document.querySelector(".js-selectCurrency");
-let currencyRateElement = document.querySelector(".js-currencyRate");
+{
+    const updateRateValue = () => {
+        const selectCurrencyElement = document.querySelector(".js-selectCurrency");
+        const eurRate = 4.71;
+        const plnRate = 0.21;
 
-let eurRate = 4.65;
-let plnRate = 0.21;
-
-currencyRateElement.innerText = `1 EUR = ${eurRate} PLN`;
-
-selectCurrencyElement.addEventListener("click", () => {
-    currencyRateElement.innerText = (selectCurrencyElement.value === "eur") ? `1 EUR = ${eurRate} PLN` : `1 PLN = ${plnRate} EUR`;
-})
-
-formElement.addEventListener("input", () => {
-    let inputAmount = inputAmountElement.value;
-    let rate;
-    let outputCurrency;
-
-    switch (selectCurrencyElement.value) {
-        case "eur":
-            rate = eurRate;
-            outputCurrency = "PLN";
-            break;
-
-        case "pln":
-            rate = plnRate;
-            outputCurrency = "EUR";
-            break;
+        return (selectCurrencyElement.value === "eur") ? eurRate : plnRate;
     }
 
-    result = inputAmount * rate;
-    resultElement.innerText = (result < 0) ? `0.00 ${outputCurrency}` : `${result.toFixed(2)} ${outputCurrency}`;
-})
+    const updateCurrency = () => {
+        const selectCurrencyElement = document.querySelector(".js-selectCurrency");
+
+        return (selectCurrencyElement.value === "eur") ? "PLN" : "EUR";
+    }
+
+    const setRateText = () => {
+        const currencyRateElement = document.querySelector(".js-currencyRate");
+
+        currencyRateElement.innerText = (updateCurrency() === "EUR") ? `1 PLN = ${updateRateValue()} EUR` : `1 EUR = ${updateRateValue()} PLN`;
+    }
+
+    const calculateResult = () => {
+        const resultElement = document.querySelector(".js-result");
+        const inputAmountElement = document.querySelector(".js-inputAmount");
+        const inputAmount = inputAmountElement.value;
+
+        const result = inputAmount * updateRateValue();
+        resultElement.innerText = (result < 0) ? `0.00 ${updateCurrency()}` : `${result.toFixed(2)} ${updateCurrency()}`;
+    }
+
+    const init = () => {
+        const selectCurrencyElement = document.querySelector(".js-selectCurrency");
+        const formElement = document.querySelector(".js-form");
+
+        setRateText();
+
+        selectCurrencyElement.addEventListener("click", setRateText);
+        formElement.addEventListener("input", calculateResult);
+    }
+
+    init();
+}
